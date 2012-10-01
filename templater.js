e9815f6a7c5c678bd245c43f2fcf3aa2300359cf
Nerudo Mregi <neudomregi@gmail.com>
2012-10-01 10:51:27 +0200

    First Commit
    Adding all the files to the project

---
+++ templater.js
@@ -0,0 +1,10 @@
+﻿//Code referenced from http://trentrichardson.com
+(function($) {
+    $.templater = function(str,replacements){
+        $.each(replacements,function(i){
+            str = str.replace(new RegExp('{'+ i +'}','g'),this);
+        });
+        return str;
+    }    
+
+})(jQuery);
